f57940a33303684790f6735cdbc3f522a9443690
Bhuvaneshwaran M <bhuvaneshmagesh16@gmail.com>
2026-09-24 22:07:59 +0530

    ci/cd pipeline for learning using factorial

---
+++ factorial.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    if n<0:
+        raise ValueError("Factorial Can't handle negative numbers")
+    fact=1
+    for i in range(1,n+1):
+        fact*=i
+    return fact
---
+++ test_factorial.py
@@ -0,0 +1,6 @@
+from factorial import factorial
+
+def test_factorial_0():
+    assert factorial(5)==120
+def test_factorial_1():
+    assert factorial(20) == 2432902008176640000
