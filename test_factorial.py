cc47c8f1d615fcdd14b5fd7fe2ec31058e2455b9
Bhuvaneshwaran M <bhuvaneshmagesh16@gmail.com>
2026-09-25 09:48:21 +0530

    updates in test_factorial file3

--- test_factorial.py
+++ test_factorial.py
@@ -10,7 +10,7 @@ def test_factorial_2():
     with pytest.raises(ValueError):
         factorial(-1)
 def test_factorial_3():
-    assert factorial(52) == 80658175170943878571660636856403766975289505440883277823999999999999
+    assert factorial(52) == 80658175170943878571660636856403766975289505440883277824000000000000
 def test_factorial_4():
     with pytest.raises(ValueError):
         factorial(-100)
