2d77f7098ab5594b468640c105e2d1dd8f5f14df
Bhuvaneshwaran M <bhuvaneshmagesh16@gmail.com>
2026-09-25 09:42:08 +0530

    updates in test_factorial file2

--- test_factorial.py
+++ test_factorial.py
@@ -1,3 +1,5 @@
+import pytest
+
 from factorial import factorial
 
 def test_factorial_0():
@@ -5,6 +7,10 @@ def test_factorial_0():
 def test_factorial_1():
     assert factorial(20) == 2432902008176640000
 def test_factorial_2():
-    assert factorial(-1) == ValueError("Factorial Can't handle negative numbers")
+    with pytest.raises(ValueError):
+        factorial(-1)
 def test_factorial_3():
     assert factorial(52) == 80658175170943878571660636856403766975289505440883277823999999999999
+def test_factorial_4():
+    with pytest.raises(ValueError):
+        factorial(-100)
